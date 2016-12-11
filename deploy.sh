00ef53d160a21eb9a2b8df4dc1f8a002caacbc9f
Ben Zhang <benzh@cs.berkeley.edu>
2016-12-10 20:17:02 -0800

    fix: add image first

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 set -x
 
 npm run build
+cp -r img build/
 
 git checkout -B gh-pages
 git add -f build
-cp -r img build/
 git commit -am "Rebuild website"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
