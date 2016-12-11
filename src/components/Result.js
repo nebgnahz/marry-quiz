e103d7a84878782c644e23b42f45f4c196eb07d7
Ben Zhang <benzh@cs.berkeley.edu>
2016-12-10 18:03:42 -0800

    fix: react-share causes issue with npm run build
    Might be due to some babel issue...

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,31 +1,5 @@
 import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import {
-  ShareButtons,
-  ShareCounts,
-  generateShareIcon
-} from 'react-share';
-
-const FacebookIcon = generateShareIcon('facebook');
-const TwitterIcon = generateShareIcon('twitter');
-const GooglePlusIcon = generateShareIcon('google');
-const LinkedinIcon = generateShareIcon('linkedin');
-const PinterestIcon = generateShareIcon('pinterest');
-
-const {
-  FacebookShareButton,
-  GooglePlusShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-  PinterestShareButton,
-} = ShareButtons;
-
-const {
-  FacebookShareCount,
-  GooglePlusShareCount,
-  LinkedinShareCount,
-  PinterestShareCount,
-} = ShareCounts;
 
 const shareUrl = 'https://ladymarry.com';
 const title = 'LadyMarry';
@@ -48,92 +22,11 @@ function Result(props) {
         <p>
           Sharing your quiz result with friends!
         </p>
-
-        <div className="share-container">
-          <div className="share">
-            <FacebookShareButton
-                url={shareUrl}
-                title={title}
-                className="share-button">
-              <FacebookIcon size={32} round />
-            </FacebookShareButton>
-            <FacebookShareCount
-                url={shareUrl}
-                className="share-count">
-              {count => count}
-            </FacebookShareCount>
-          </div>
-
-          <div className="share">
-            <div>
-              <TwitterShareButton
-                  url={shareUrl}
-                  title={title}
-                  className="share-button">
-                <TwitterIcon
-                    size={32}
-                    round />
-              </TwitterShareButton>
-              <div className="share-count">
-                &nbsp;
-              </div>
-            </div>
-          </div>
-
-          <div className="share">
-            <div>
-              <GooglePlusShareButton
-                  url={shareUrl}
-                  title={title}
-                  className="share-button">
-                <GooglePlusIcon
-                    size={32}
-                    round />
-              </GooglePlusShareButton>
-              <GooglePlusShareCount
-                  url={shareUrl}
-                  className="share-count">
-                {count => count}
-              </GooglePlusShareCount>
-            </div>
-          </div>
-
-          <div className="share">
-            <div>
-              <LinkedinShareButton
-                  url={shareUrl}
-                  title={title}
-                  className="share-button">
-                <LinkedinIcon
-                    size={32}
-                    round />
-              </LinkedinShareButton>
-              <LinkedinShareCount
-                  url={shareUrl}
-                  className="share-count">
-                {count => count}
-              </LinkedinShareCount>
-            </div>
-          </div>
-
-          <div className="share">
-            <div>
-              <PinterestShareButton
-                  url={shareUrl}
-                  title={title}
-                  className="share-button">
-                <PinterestIcon
-                    size={32}
-                    round />
-              </PinterestShareButton>
-              <PinterestShareCount
-                  url={shareUrl}
-                  className="share-count">
-                {count => count}
-              </PinterestShareCount>
-            </div>
-
-          </div>
+        <div className="fb-share-button" data-href={shareUrl}
+             data-layout="box_count" data-size="small" data-mobile-iframe="true">
+          <a className="fb-xfbml-parse-ignore" target="_blank"
+             href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fladymarry.com%2F&amp;src=sdkpreparse">Share
+          </a>
         </div>
       </div>
     </ReactCSSTransitionGroup>
